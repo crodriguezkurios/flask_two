0908a1438093fcd512172b13da3f8bf6414a2768
crodriguezkurios <crodriguez.kuriosedu@gmail.com>
2024-10-21 11:28:07 -0400

    new courses view

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world(request):
-    name = request.args.get('name', 'World')
+def get_courses(id=None):
+
+    courses = ['bb-block', 
+               'bb-block i', 
+               'bb-block ii', 
+               'bb-block iii', 
+               'makerlab i', 
+               'makerlab ii', 
+               'makerlab iii']
     
-    return 'Hello, {}!'.format(name)
+    if id is not None:
+        return courses[int(id)]
+    
+    return courses
+
+
+@app.route('/')
+def hello_world():
+
+    return render_template('index.html', data={'list_num': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]})
 
 @app.route('/courses')
 def courses():
-    return 'Cursos'
+
+    courses = get_courses()
+    
+    return render_template('courses.html', data={'courses': courses})
+
+@app.route('/course/<id>')
+def course(id):
+
+    course = get_courses(id) 
+
+    return render_template('course.html', data={'course': course})
+
+
+app.run(debug=True)
